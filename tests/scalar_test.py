b384b408d23401a6bbbe70e89da133e40f04b962
Shivendra <shivharsh44@gmail.com>
2024-10-02 13:49:07 +0530

    updated tests

--- tests/scalar_test.py
+++ tests/scalar_test.py
@@ -1,71 +1,81 @@
-"""
-  doesn't has assert functions, just manually match the outputs to the rest
-"""
-
+import unittest
 import torch
-
-x1, x2 = torch.tensor([2.0], requires_grad=True), torch.tensor([3.0], requires_grad=True)
-x3, x4 = torch.tensor([5.0], requires_grad=True), torch.tensor([10.0], requires_grad=True)
-x5, x6 = torch.tensor([1.0], requires_grad=True), torch.tensor([4.0], requires_grad=True)
-x7 = torch.tensor([-2.0], requires_grad=True)
-
-a1 = x1 + x2
-a2 = x3 - x4
-a3 = a1 * a2
-a4 = a3 ** 2
-a5 = x5 * x6
-a6 = a5.sigmoid()
-a7 = x7.tanh()
-a8 = a4 + a6
-a9 = a8 + a7
-y = a9.relu()
-
-a1.retain_grad()
-a2.retain_grad()
-a3.retain_grad()
-a4.retain_grad()
-a5.retain_grad()
-a6.retain_grad()
-a7.retain_grad()
-a8.retain_grad()
-a9.retain_grad()
-y.retain_grad()
-y.backward()
-
-print(x1.data, x1.grad)
-print(x2.data, x2.grad)
-print(x3.data, x3.grad)
-print(x4.data, x4.grad)
-print(x5.data, x5.grad)
-print(x6.data, x6.grad)
-print(x7.data, x7.grad)
-print(y.data, y.grad)
-
 from drop import scalar
 
-x1, x2 = scalar(2), scalar(3)
-x3, x4 = scalar(5), scalar(10)
-x5, x6 = scalar(1), scalar(4)
-x7 = scalar(-2)
+class TestScalarAutograd(unittest.TestCase):
+  
+  def setUp(self):
+    # Setting up initial values for PyTorch tensors
+    self.x1_torch = torch.tensor([2.0], requires_grad=True)
+    self.x2_torch = torch.tensor([3.0], requires_grad=True)
+    self.x3_torch = torch.tensor([5.0], requires_grad=True)
+    self.x4_torch = torch.tensor([10.0], requires_grad=True)
+    self.x5_torch = torch.tensor([1.0], requires_grad=True)
+    self.x6_torch = torch.tensor([4.0], requires_grad=True)
+    self.x7_torch = torch.tensor([-2.0], requires_grad=True)
 
-a1 = x1 + x2
-a2 = x3 - x4
-a3 = a1 * a2
-a4 = a3 ** 2
-a5 = x5 * x6
-a6 = a5.sigmoid()
-a7 = x7.tanh()
-a8 = a4 + a6
-a9 = a8 + a7
-y = a9.relu()
+    # Setting up initial values for drop.scalars
+    self.x1_drop = scalar(2)
+    self.x2_drop = scalar(3)
+    self.x3_drop = scalar(5)
+    self.x4_drop = scalar(10)
+    self.x5_drop = scalar(1)
+    self.x6_drop = scalar(4)
+    self.x7_drop = scalar(-2)
 
-y.backward()
+  def test_forward_and_backward(self):
+    # --- PyTorch Computation ---
+    a1_torch = self.x1_torch + self.x2_torch
+    a2_torch = self.x3_torch - self.x4_torch
+    a3_torch = a1_torch * a2_torch
+    a4_torch = a3_torch ** 2
+    a5_torch = self.x5_torch * self.x6_torch
+    a6_torch = a5_torch.sigmoid()
+    a7_torch = self.x7_torch.tanh()
+    a8_torch = a4_torch + a6_torch
+    a9_torch = a8_torch + a7_torch
+    y_torch = a9_torch.relu()
 
-print(x1)
-print(x2)
-print(x3)
-print(x4)
-print(x5)
-print(x6)
-print(x7)
-print(y)
+    # Retaining gradients for PyTorch intermediate values
+    a1_torch.retain_grad()
+    a2_torch.retain_grad()
+    a3_torch.retain_grad()
+    a4_torch.retain_grad()
+    a5_torch.retain_grad()
+    a6_torch.retain_grad()
+    a7_torch.retain_grad()
+    a8_torch.retain_grad()
+    a9_torch.retain_grad()
+    y_torch.retain_grad()
+    
+    y_torch.backward()
+    
+    # --- Drop Scalar Computation ---
+    a1_drop = self.x1_drop + self.x2_drop
+    a2_drop = self.x3_drop - self.x4_drop
+    a3_drop = a1_drop * a2_drop
+    a4_drop = a3_drop ** 2
+    a5_drop = self.x5_drop * self.x6_drop
+    a6_drop = a5_drop.sigmoid()
+    a7_drop = self.x7_drop.tanh()
+    a8_drop = a4_drop + a6_drop
+    a9_drop = a8_drop + a7_drop
+    y_drop = a9_drop.relu()
+
+    y_drop.backward()
+
+    # --- Assertions ---
+    # 1. Compare forward outputs
+    self.assertAlmostEqual(y_torch.item(), y_drop.item(), places=5, msg="Forward outputs do not match!")
+
+    # 2. Compare gradients
+    self.assertAlmostEqual(self.x1_torch.grad.item(), self.x1_drop.grad, places=5, msg="Gradient mismatch for x1")
+    self.assertAlmostEqual(self.x2_torch.grad.item(), self.x2_drop.grad, places=5, msg="Gradient mismatch for x2")
+    self.assertAlmostEqual(self.x3_torch.grad.item(), self.x3_drop.grad, places=5, msg="Gradient mismatch for x3")
+    self.assertAlmostEqual(self.x4_torch.grad.item(), self.x4_drop.grad, places=5, msg="Gradient mismatch for x4")
+    self.assertAlmostEqual(self.x5_torch.grad.item(), self.x5_drop.grad, places=5, msg="Gradient mismatch for x5")
+    self.assertAlmostEqual(self.x6_torch.grad.item(), self.x6_drop.grad, places=5, msg="Gradient mismatch for x6")
+    self.assertAlmostEqual(self.x7_torch.grad.item(), self.x7_drop.grad, places=5, msg="Gradient mismatch for x7")
+
+if __name__ == '__main__':
+  unittest.main()
--- tests/tensor_test.py
+++ tests/tensor_test.py
@@ -1,67 +1,69 @@
-from drop import tensor
-
-a = tensor([[2, 4, 5, -4], [-3, 0, 9, -1]])
-b = tensor([[1, 0, -2, 0], [-1, 10, -2, 4]])
-
-c = a + b
-d = c.tanh()
-e = d.silu()
-f = e ** 2
-g = f.sigmoid()
-h = g.sum()
-
-h.backward()
-
-print("a.grad:")
-print(a.grad)
-print("\nb.grad:")
-print(b.grad)
-print("\nc.grad:")
-print(c.grad)
-print("\nd.grad:")
-print(d.grad)
-print("\ne.grad:")
-print(e.grad)
-print("\nf.grad:")
-print(f.grad)
-print("\ng.grad:")
-print(g.grad)
-print("\nh.grad:")
-print(h.grad)
-
+import unittest
 import torch
+from drop import tensor
+import numpy as np
 
-a = torch.tensor([[2.0, 4.0, 5.0, -4.0], [-3.0, 0.0, 9.0, -1.0]], requires_grad=True)
-b = torch.tensor([[1.0, 0.0, -2.0, 0.0], [-1.0, 10.0, -2.0, 4.0]], requires_grad=True)
+class TestDropTensorAutograd(unittest.TestCase):
+  
+  def setUp(self):
+    # Setting up initial values for drop.tensor
+    self.a_drop = tensor([[2, 4, 5, -4], [-3, 0, 9, -1]])
+    self.b_drop = tensor([[1, 0, -2, 0], [-1, 10, -2, 4]])
 
-c = a + b
-d = torch.tanh(c)
-e = torch.nn.functional.silu(d)
-f = e ** 2
-g = torch.sigmoid(f)
-h = g.sum()
+    # Setting up initial values for PyTorch tensors
+    self.a_torch = torch.tensor([[2.0, 4.0, 5.0, -4.0], [-3.0, 0.0, 9.0, -1.0]], requires_grad=True)
+    self.b_torch = torch.tensor([[1.0, 0.0, -2.0, 0.0], [-1.0, 10.0, -2.0, 4.0]], requires_grad=True)
 
-c.retain_grad()
-d.retain_grad()
-e.retain_grad()
-f.retain_grad()
-g.retain_grad()
-h.retain_grad()
-h.backward()
+  def test_forward_and_backward(self):
+    # --- Drop Tensor Computation ---
+    c_drop = self.a_drop + self.b_drop
+    d_drop = c_drop.tanh()
+    e_drop = d_drop.silu()
+    f_drop = e_drop ** 2
+    g_drop = f_drop.sigmoid()
+    h_drop = g_drop.sum()
+    
+    h_drop.backward()
 
-print("a.grad:")
-print(a.grad)
-print("\nb.grad:")
-print(b.grad)
-print("\nc.grad:")
-print(c.grad)
-print("\nd.grad:")
-print(d.grad)
-print("\ne.grad:")
-print(e.grad)
-print("\nf.grad:")
-print(f.grad)
-print("\ng.grad:")
-print(g.grad)
-print("\nh.grad:")
-print(h.grad)
+    # --- PyTorch Computation ---
+    c_torch = self.a_torch + self.b_torch
+    d_torch = torch.tanh(c_torch)
+    e_torch = torch.nn.functional.silu(d_torch)
+    f_torch = e_torch ** 2
+    g_torch = torch.sigmoid(f_torch)
+    h_torch = g_torch.sum()
+
+    # Retain gradients for all intermediate PyTorch tensors
+    c_torch.retain_grad()
+    d_torch.retain_grad()
+    e_torch.retain_grad()
+    f_torch.retain_grad()
+    g_torch.retain_grad()
+    h_torch.retain_grad()
+    
+    h_torch.backward()
+
+    # --- Assertions ---
+    # Compare forward outputs
+    self.assertAlmostEqual(h_drop.item(), h_torch.item(), places=5, msg="Sum outputs do not match!")
+    
+    # Compare gradients for all tensors
+    self.assertTrue(self._compare_grads(self.a_drop.grad, self.a_torch.grad), msg="Gradient mismatch for 'a'")
+    self.assertTrue(self._compare_grads(self.b_drop.grad, self.b_torch.grad), msg="Gradient mismatch for 'b'")
+    self.assertTrue(self._compare_grads(c_drop.grad, c_torch.grad), msg="Gradient mismatch for 'c'")
+    self.assertTrue(self._compare_grads(d_drop.grad, d_torch.grad), msg="Gradient mismatch for 'd'")
+    self.assertTrue(self._compare_grads(e_drop.grad, e_torch.grad), msg="Gradient mismatch for 'e'")
+    self.assertTrue(self._compare_grads(f_drop.grad, f_torch.grad), msg="Gradient mismatch for 'f'")
+    self.assertTrue(self._compare_grads(g_drop.grad, g_torch.grad), msg="Gradient mismatch for 'g'")
+    self.assertTrue(self._compare_grads(h_drop.grad, h_torch.grad), msg="Gradient mismatch for 'h'")
+
+  def _compare_grads(self, drop_grad, torch_grad, tol=1e-5):
+    """
+    Helper function to compare gradients between drop.tensor and PyTorch tensors.
+    """
+    if drop_grad is None or torch_grad is None:
+      return drop_grad is None and torch_grad is None
+    return np.allclose(drop_grad.tolist(), torch_grad.detach().numpy(), atol=tol)
+
+if __name__ == '__main__':
+  unittest.main()
