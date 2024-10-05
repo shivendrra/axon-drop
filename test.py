e9527ad5af836cd18a3a0f818e82207806a0873c
Shivendra <shivharsh44@gmail.com>
2024-10-05 10:13:24 +0530

    added `mlp` & `rnn` in example

--- test.py
+++ test.py
@@ -1,31 +1,32 @@
 import drop
 import drop.nn as nn
 
-# Input and target tensors
-xs = drop.tensor([
-  [2.0, 3.0, -1.0],
-  [3.0, 0.0, -0.5],
-  [0.5, 1.0, 1.0],
-  [1.0, 1.0, -1.0]
-], requires_grad=True)
-ys = drop.tensor([1.0, -1.0, -1.0, 1.0], requires_grad=True)
-
-# Define the model
-class MLP(nn.Module):
+class RNN(nn.Module):
   def __init__(self, _in, _hidden, _out) -> None:
     super().__init__()
-    self.layer1 = nn.Linear(_in, _hidden, bias=False)
+    self.hidden_size = _hidden
+    self.i2h = nn.Linear(_in, _hidden, bias=False)
+    self.h2h = nn.Linear(_hidden, _hidden, bias=False)
+    self.h2o = nn.Linear(_hidden, _out, bias=False)
     self.tanh = nn.Tanh()
-    self.layer2 = nn.Linear(_hidden, _out, bias=False)
   
   def forward(self, x):
-    out = self.layer1(x)
-    out = self.tanh(out)
-    out = self.layer2(out)
+    batch_size = x.shape[0]
+    h = drop.zeros(shape=(batch_size, self.hidden_size))
+    for t in range(x.shape[1]):
+      h = self.tanh(self.i2h(x[:, t]) + self.h2h(h))
+    out = self.h2o(h)
     return out
 
-# Initialize model
-model = MLP(3, 10, 1)
+xs = drop.tensor([
+  [[2.0, 3.0, -1.0]],
+  [[3.0, 0.0, -0.5]],
+  [[0.5, 1.0, 1.0]],
+  [[1.0, 1.0, -1.0]]
+], requires_grad=True)  # Shape: [batch_size, time_steps, input_features]
+
+ys = drop.tensor([[1.0], [-1.0], [-1.0], [1.0]], requires_grad=True)  # Shape: [batch_size, output_features]
+model = RNN(3, 10, 1)
 epochs = 100
 learning_rate = 0.01
 
@@ -36,5 +37,7 @@ for k in range(epochs):
   model.zero_grad()
   loss.backward()
   print(f"Epoch {k}: Loss = {loss.data}")
+  
+  # Update parameters
   for p in model.parameters():
     p.data -= p.grad * learning_rate
