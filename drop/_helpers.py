e36ed9af7c613f004c7fd977a223e612a669299c
Shivendra <shivharsh44@gmail.com>
2025-02-02 18:31:05 +0530

    turned ``helper`` dir's codes into a single file

---
+++ drop/_helpers.py
@@ -0,0 +1,62 @@
+def get_shape(data):
+  if isinstance(data, list):
+    return [len(data), ] + get_shape(data[0])
+  return []
+
+def flatten(data):
+  if isinstance(data, list):
+    return [item for sublist in data for item in flatten(sublist)]
+  return [data]
+
+def flatten_recursive(data:list, start_dim:int=0, end_dim:int=-1) -> list:
+  def _recurse_flatten(data, current_dim):
+    if current_dim < start_dim:
+      return [_recurse_flatten(item, current_dim + 1) for item in data]
+    elif start_dim <= current_dim <= end_dim:
+      return flatten(data)
+    else:
+      return data
+  if end_dim == -1:
+    end_dim = len(get_shape(data)) - 1
+  return _recurse_flatten(data, 0)
+
+def reshape(data:list, new_shape:tuple) -> list:
+  assert type(new_shape) == tuple, "new shape must be a tuple"
+  def _shape_numel(shape):
+    numel = 1
+    for dim in shape:
+      numel *= dim
+    return numel
+
+  def unflatten(flat, shape):
+    if len(shape) == 1:
+      return flat[:shape[0]]
+    size = shape[0]
+    return [unflatten(flat[i*int(len(flat)/size):(i+1)*int(len(flat)/size)], shape[1:]) for i in range(size)]
+
+  def infer_shape(shape, total_size):
+    if shape.count(-1) > 1:
+      raise ValueError("Only one dimension can be -1")
+    unknown_dim, known_dims, known_size = shape.index(-1) if -1 in shape else None, [dim for dim in shape if dim != -1], 1
+    for dim in known_dims:
+      known_size *= dim      
+    if unknown_dim is not None:
+      inferred_size = total_size // known_size
+      if inferred_size * known_size != total_size:
+        raise ValueError(f"Cannot reshape array to shape {shape}")
+      shape = list(shape)
+      shape[unknown_dim] = inferred_size
+    return shape
+  original_size = _shape_numel(get_shape(data))
+  new_shape, new_size = infer_shape(new_shape, original_size), _shape_numel(new_shape)
+  if original_size != new_size:
+    raise ValueError(f"Cannot reshape array of size {original_size} to shape {new_shape}")
+  flat_data = flatten(data)
+  return unflatten(flat_data, new_shape)
+
+def _zeros(shape):
+  if not shape:
+    return [0]
+  if len(shape) == 1:
+    return [0] * shape[0]
+  return [_zeros(shape[1:]) for _ in range(shape[0])]
--- drop/_tensor.py
+++ drop/_tensor.py
@@ -2,13 +2,13 @@ import ctypes
 from ._core import CScalar, CTensor, libtensor
 from ._core import DTYPE_FLOAT32, DTYPE_FLOAT64, DTYPE_INT16, DTYPE_INT32, DTYPE_INT64, DTYPE_INT8
 from typing import *
-from .helpers.shape import flatten, get_shape
+from ._helpers import flatten, get_shape
 
 int8, int16, int32, int64, float32, float64 = DTYPE_INT8, DTYPE_INT16, DTYPE_INT32, DTYPE_INT64, DTYPE_FLOAT32, DTYPE_FLOAT64
 
 class tensor:
   int8, int16, int32, int64, float32, float64 = int8, int16, int32, int64, float32, float64
-  def __init__(self, data=None, dtype:Optional[Literal['int8', 'int16', 'int32', 'int64', 'float32', 'float64']]=None, device:Optional[Literal['cuda', 'cpu']]='cpu', requires_grad:bool=False):
+  def __init__(self, data=None, dtype:Optional[Literal['int8', 'int16', 'int32', 'int64', 'float32', 'float64']]=None, requires_grad:bool=False):
     if data is not None:
       data, shape, dtype = flatten(data), get_shape(data), dtype if dtype else float32
       
@@ -16,34 +16,27 @@ class tensor:
       self._shape_ctype = (ctypes.c_int * len(shape))(*shape.copy())
       self._dtype_ctype = ctypes.c_int(dtype)
       self._ndim_ctype = ctypes.c_int(len(shape))
-      self._device_ctype = device.encode('utf-8')
 
-      self.shape, self.ndim, self.device = shape.copy(), len(shape), device
+      self.shape, self.ndim = shape.copy(), len(shape)
       self.numel = 1
       self.dtype = dtype
       for i in self.shape:
         self.numel *= i
-      
+
       self.requires_grads = requires_grad
-      self.hooks = []
-      self.grad, self.grad_fn = None, None
-      self.tensor = libtensor.create_tensor(self._data_ctype, self._shape_ctype, self._ndim_ctype, self._device_ctype, self._dtype_ctype)
+      self.tensor = libtensor.create_tensor(self._data_ctype, self._shape_ctype, self._ndim_ctype, self._dtype_ctype)
     else:
-      self.tensor, self.shape, self.ndim, self.device, self.requires_grads, self.dtype = None, None, None, device, None, None
-      self.hooks, self.grad, self.grad_fn = [], None, None
+      self.tensor, self.shape, self.ndim, self.requires_grads, self.dtype = None, None, None, None, None
 
   def __del__(self):
     libtensor.delete_strides(self.tensor)
     libtensor.delete_backstrides(self.tensor)
     libtensor.delete_shape(self.tensor)
-    libtensor.delete_device(self.tensor)
-    libtensor.delete_aux(self.tensor)
     libtensor.delete_data(self.tensor)
     libtensor.delete_tensor(self.tensor)
   
-  def to(self, device:str):
-    device = str(device)
-    self.device = device
-    self._device_ctype = self.device.encode('utf-8')
-    libtensor.to_device(self.tensor, self._device_ctype)
-    return self
+  def __str__(self):
+    libtensor.print_tensor(self.tensor)
+
+  def __add__(self, other):
+    pass
